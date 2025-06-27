1360d74eef5ab44e46c176d8ed6a428416a9586d
mft <70207320+PLCwithoutP@users.noreply.github.com>
2025-06-27 20:48:15 +0300

    Section 14.2 Else-if is added

--- section14/14.2_ElseIf/main.cpp
+++ section14/14.2_ElseIf/main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
 
+// Tools
+const int Pen {10};
+const int Marker {20};
+const int Eraser {30};
+const int Rectangle {40};
+const int Circle {50};
+const int Ellipse {60};
+
 int main(int argc, char **argv)
 {
+    int tool {Circle};
 
+    if (tool == Pen)
+    {
+        std::cout << "Active tool is pen" << std::endl;
+    }
+    else if (tool == Marker)
+    {
+        std::cout << "Active tool is marker" << std::endl;
+    }
+    else if (tool == Eraser)
+    {
+        std::cout << "Active tool is eraser" << std::endl;
+    }
+    else if (tool == Rectangle)
+    {
+        std::cout << "Active tool is rectangle" << std::endl;
+    }
+    else if (tool == Circle)
+    {
+        std::cout << "Active tool is circle" << std::endl;
+    }
+    else if (tool == Ellipse)
+    {
+        std::cout << "Active tool is ellipse" << std::endl;
+    }
+
+    std::cout << "Moving on..." << std::endl;
+    
     return 0;
 }
